f66748cf00c6fa5f05e70545bcf9a36871ad63b4
TopScrech <sergei_saliukov@icloud.com>
2024-09-26 14:26:26 +0200

    Initial Commit

---
+++ Dickpic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dickpic
+//
+//  Created by Sergei Saliukov on 26/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dickpic/DickpicApp.swift
@@ -0,0 +1,17 @@
+//
+//  DickpicApp.swift
+//  Dickpic
+//
+//  Created by Sergei Saliukov on 26/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DickpicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
