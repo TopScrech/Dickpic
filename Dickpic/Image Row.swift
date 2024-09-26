eebd279a146bd9f7f1294fc9e6e6db1eb3a6e0fa
TopScrech <sergei_saliukov@icloud.com>
2024-09-26 16:08:46 +0200

    Added blur

--- Dickpic/Image Row.swift
+++ Dickpic/Image Row.swift
@@ -14,14 +14,21 @@ struct ImageRow: View {
         UIImage(cgImage: image)
     }
     
+    @State private var isHidden = true
+    
     var body: some View {
         Image(uiImage: uiImage)
             .resizable()
             .frame(width: 100, height: 100)
             .scaledToFit()
             .clipped()
+            .blur(radius: isHidden ? 5 : 0)
+            .animation(.default, value: isHidden)
             .overlay(alignment: .bottomTrailing) {
                 Image(systemName: "eye.slash")
+            }
+            .onTapGesture {
+                isHidden.toggle()
             }
     }
 }
