934f2fcc050c9bf287b798e59b3792a29b96b4f5
TopScrech <sergei_saliukov@icloud.com>
2025-06-04 08:39:20 +0200

    adopted Photo Library View for macOS

--- Dickpic/Photo Library View.swift
+++ Dickpic/Photo Library View.swift
@@ -5,12 +5,16 @@ struct PhotoLibraryView: View {
     @EnvironmentObject private var store: ValueStore
     
     private static let initialColumns = 3
-    @State private var gridColumns = Array(repeating: GridItem(.flexible()), count: initialColumns)
+    
+    @State private var gridColumns = Array(
+        repeating: GridItem(.flexible()),
+        count: initialColumns
+    )
     
     var body: some View {
         VStack {
             if vm.deniedAccess {
-                Text("Access to the photo library has been denied. Please enable access in settings.")
+                Text("Access to the photo library has been denied. Please enable access in settings")
             } else {
                 ScrollView {
                     LazyVGrid(columns: gridColumns) {
@@ -19,7 +23,11 @@ struct PhotoLibraryView: View {
                         }
                         
                         ForEach(vm.sensitiveVideos, id: \.self) { videoUrl in
+#if os(macOS)
+                            Text(videoUrl.description)
+#else
                             VideoRow(videoUrl)
+#endif
                         }
                     }
                     .animation(.default, value: vm.totalAssets)
@@ -38,6 +46,7 @@ struct PhotoLibraryView: View {
                 Text("Total Assets: `\(vm.totalPhotos)` (`\(vm.processedAssets)` processed)")
                     .animation(.default, value: vm.totalPhotos)
                     .animation(.default, value: vm.processedAssets)
+                    .numericTransition()
                 
                 ProgressButton("Analyze", progress: vm.progress) {
                     Task {
@@ -45,7 +54,13 @@ struct PhotoLibraryView: View {
                     }
                 }
             }
+#if os(macOS)
+            .padding(8)
+            .padding(.vertical, 8)
+            .buttonStyle(.plain)
+#else
             .padding(.bottom, 5)
+#endif
         }
     }
 }
