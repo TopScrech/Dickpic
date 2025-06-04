e790853a52d34cd98b393ae8acdf44ce6231f51f
TopScrech <sergei_saliukov@icloud.com>
2025-06-04 13:48:54 +0200

    -

--- Dickpic/Photo Library View.swift
+++ Dickpic/Photo Library View.swift
@@ -79,6 +79,27 @@ struct PhotoLibraryView: View {
                     }
                 }
                 .disabled(vm.isProcessing && vm.progress > 0.95)
+                .contextMenu {
+                    Button {
+                        Task {
+                            await vm.fetchAssets(
+                                analyzeConcurrently: true
+                            )
+                        }
+                    } label: {
+                        Label("Analyze Concurrently", systemImage: "square.grid.3x3")
+                    }
+                    
+                    Button {
+                        Task {
+                            await vm.fetchAssets(
+                                analyzeConcurrently: false
+                            )
+                        }
+                    } label: {
+                        Label("Analyze Sequentually", systemImage: "square")
+                    }
+                }
             }
             .monospacedDigit()
 #if os(macOS)
